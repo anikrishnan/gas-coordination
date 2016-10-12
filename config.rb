bf3488a50ff98e7362d324f9fbc9682195ff41fc
Matt Gardner <wilburnforce@gmail.com>
2016-10-12 14:25:12 -0400

    CHangin structure

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/string/inflections"
 ###
 # Page options, layouts, aliases and proxies
 ###
-
+activate :directory_indexes
 # Per-page layout changes:
 #
 # With no layout
@@ -15,12 +15,12 @@ page '/*.txt', layout: false
 page '/', layout: 'story' # Use story layout for index page
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-data.categories.each do |category|
-  str = "/categories/#{category.name.parameterize.downcase}"
-  puts "Generating page #{str}"
-  proxy "/categories/#{category.name.parameterize.downcase}/index.html",
-    "/categories/template.html", locals: { category: category }, ignore: true
-end
+# data.categories.each do |category|
+#   str = "/categories/#{category.name.parameterize.downcase}"
+#   puts "Generating page #{str}"
+#   proxy "/categories/#{category.name.parameterize.downcase}/index.html",
+#     "/categories/template.html", locals: { category: category }, ignore: true
+# end
 # General configuration
 
 # Reload the browser automatically whenever files change
@@ -36,41 +36,41 @@ end
 helpers do
   extend Haml::Helpers
 
-  def link_to(resource_name, resource_or_param, param_name: nil, &block)
-    path = path_to(resource_name, resource_or_param, param_name: param_name)
-    if block_given?
-      haml_tag :a, href: path do
-        yield
-      end
-    else
-      haml_tag :a, href: path
-    end
-  end
+  # def link_to(resource_name, resource_or_param, param_name: nil, &block)
+  #   path = path_to(resource_name, resource_or_param, param_name: param_name)
+  #   if block_given?
+  #     haml_tag :a, href: path do
+  #       yield
+  #     end
+  #   else
+  #     haml_tag :a, href: path
+  #   end
+  # end
 
-  def markdown(content)
-    @markdown ||= Redcarpet::Markdown.new(
-      Redcarpet::Render::HTML,
-      autolink: true,
-      filter_html: true
-    )
-    @markdown.render(content)
-  end
+  # def markdown(content)
+  #   @markdown ||= Redcarpet::Markdown.new(
+  #     Redcarpet::Render::HTML,
+  #     autolink: true,
+  #     filter_html: true
+  #   )
+  #   @markdown.render(content)
+  # end
 
-  def step_markdown(*path_segments)
-    filepath = content_file(path_segments)
-    markdown File.read(filepath)
-  rescue Errno::ENOENT => e
-    if ENV.fetch('RESCUE_MISSING_FILES', true).to_b
-      markdown "> No file found at `#{filepath}`"
-    else
-      raise
-    end
-  end
+  # def step_markdown(*path_segments)
+  #   filepath = content_file(path_segments)
+  #   markdown File.read(filepath)
+  # rescue Errno::ENOENT => e
+  #   if ENV.fetch('RESCUE_MISSING_FILES', true).to_b
+  #     markdown "> No file found at `#{filepath}`"
+  #   else
+  #     raise
+  #   end
+  # end
 
-  def content_file(*path_segments, ext: '.md')
-    segments = path_segments.flatten.map { |s| s.to_s.downcase.parameterize.underscore }
-    File.join(Dir.pwd, 'source', segments) << ext
-  end
+  # def content_file(*path_segments, ext: '.md')
+  #   segments = path_segments.flatten.map { |s| s.to_s.downcase.parameterize.underscore }
+  #   File.join(Dir.pwd, 'source', segments) << ext
+  # end
 
   def render_haml(title, operations: [:to_s, :downcase, :parameterize])
     filename = operations.inject(title) { |result, method| result.send(method) }
@@ -79,15 +79,15 @@ helpers do
     Haml::Engine.new(contents).render
   end
 
-  def path_to(resource_name, resource_or_param, param_name: nil)
-    param_to_use = param_name || :id
-    segments = if [String, Fixnum].include?(resource_or_param.class)
-      [resource_name, resource_or_param]
-    else
-      [resource_name, resource_or_param.send(param_to_use)]
-    end
-    "/" << segments.map { |s| s.to_s.downcase.parameterize }.join('/')
-  end
+  # def path_to(resource_name, resource_or_param, param_name: nil)
+  #   param_to_use = param_name || :id
+  #   segments = if [String, Fixnum].include?(resource_or_param.class)
+  #     [resource_name, resource_or_param]
+  #   else
+  #     [resource_name, resource_or_param.send(param_to_use)]
+  #   end
+  #   "/" << segments.map { |s| s.to_s.downcase.parameterize }.join('/')
+  # end
 
 end
 
